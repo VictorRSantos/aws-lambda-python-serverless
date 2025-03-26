22b05a54a23945b4e8bec7c2983c06eb26e75c87
Victor R Santos <vrsisa@gmail.com>
2025-03-26 07:58:46 -0300

    Criando repositorio curso python aws-lambda

---
+++ introducao-python/programa_2.py
@@ -0,0 +1,14 @@
+# Descrição: Comentátios em Python
+
+print(10 * 6) # Multiplicação em Python
+
+# Divisão em Python
+#print(10 / 6)
+
+'''
+Comentários em Python
+print(2 + 3)
+print(2 + 4)
+
+'''
+print(5 + 3)
---
+++ introducao-python/programa_3.py
@@ -0,0 +1,19 @@
+
+# Descrição: Variaveis e tipos de dados
+
+# Variáveis em Python
+# Variáveis são usadas para armazenar valores
+
+maximo_cpu = 80
+tipo_instancia = "c5a.2xlarge"
+regiao = "us-east-1"
+
+print(maximo_cpu)
+print(tipo_instancia)
+print(regiao)
+
+maximo_cpu = maximo_cpu / 2
+print(maximo_cpu)
+
+maximo_cpu_inicial = maximo_cpu * 2
+print(maximo_cpu_inicial)
